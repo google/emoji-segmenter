30a4383a37cbd405916ec4aefac1bcace0ec11d8
Dominik Röttsches <drott@chromium.org>
2024-02-16 16:01:35 +0200

    Change operation to F1 mode
    This is equivalent to the mode used in HarfBuzz and leads to performance
    improvements in Blink's SymbolIterator.

--- emoji_presentation_scanner.c
+++ emoji_presentation_scanner.c
@@ -16,51 +16,34 @@
  */
 
 
-#line 20 "emoji_presentation_scanner.c"
-static const char _emoji_presentation_actions[] = {
-	0, 1, 0, 1, 1, 1, 5, 1, 
-	6, 1, 7, 1, 8, 1, 9, 1, 
-	10, 1, 11, 2, 2, 3, 2, 2, 
-	4
-};
-
-static const char _emoji_presentation_key_offsets[] = {
-	0, 5, 7, 14, 18, 20, 21, 24, 
-	29, 30, 34, 36
-};
-
+#line 17 "emoji_presentation_scanner.c"
 static const unsigned char _emoji_presentation_trans_keys[] = {
-	3u, 7u, 13u, 0u, 2u, 14u, 15u, 2u, 
-	3u, 6u, 7u, 13u, 0u, 1u, 9u, 10u, 
-	11u, 12u, 10u, 12u, 10u, 4u, 10u, 12u, 
-	4u, 9u, 10u, 11u, 12u, 6u, 9u, 10u, 
-	11u, 12u, 8u, 10u, 9u, 10u, 11u, 12u, 
-	14u, 0
+	0u, 13u, 14u, 15u, 0u, 13u, 9u, 12u, 10u, 12u, 10u, 10u, 4u, 12u, 4u, 12u, 
+	6u, 6u, 9u, 12u, 8u, 10u, 9u, 14u, 0
 };
 
-static const char _emoji_presentation_single_lengths[] = {
-	3, 2, 5, 4, 2, 1, 3, 5, 
-	1, 4, 2, 5
-};
-
-static const char _emoji_presentation_range_lengths[] = {
-	1, 0, 1, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0
+static const char _emoji_presentation_key_spans[] = {
+	14, 2, 14, 4, 3, 1, 9, 9, 
+	1, 4, 3, 6
 };
 
 static const char _emoji_presentation_index_offsets[] = {
-	0, 5, 8, 15, 20, 23, 25, 29, 
-	35, 37, 42, 45
+	0, 15, 18, 33, 38, 42, 44, 54, 
+	64, 66, 71, 75
 };
 
 static const char _emoji_presentation_indicies[] = {
-	2, 1, 1, 1, 0, 4, 5, 3, 
-	7, 8, 10, 11, 12, 6, 9, 5, 
-	13, 14, 15, 0, 13, 15, 16, 13, 
-	16, 15, 13, 15, 16, 15, 5, 13, 
-	14, 15, 16, 5, 17, 5, 13, 14, 
-	18, 17, 5, 13, 16, 5, 13, 14, 
-	15, 4, 16, 0
+	1, 1, 1, 2, 0, 0, 0, 1, 
+	0, 0, 0, 0, 0, 1, 0, 4, 
+	5, 3, 6, 6, 7, 8, 9, 9, 
+	10, 11, 9, 9, 9, 9, 9, 12, 
+	9, 5, 13, 14, 15, 0, 13, 16, 
+	15, 16, 13, 16, 15, 16, 16, 16, 
+	16, 16, 13, 16, 15, 16, 15, 16, 
+	16, 16, 16, 5, 13, 14, 15, 16, 
+	5, 17, 5, 13, 14, 18, 17, 5, 
+	16, 13, 16, 5, 13, 14, 15, 16, 
+	4, 16, 0
 };
 
 static const char _emoji_presentation_trans_targs[] = {
@@ -70,18 +53,18 @@ static const char _emoji_presentation_trans_targs[] = {
 };
 
 static const char _emoji_presentation_trans_actions[] = {
-	17, 19, 19, 15, 0, 7, 22, 19, 
-	19, 9, 0, 22, 19, 0, 5, 19, 
-	11, 13, 19
+	1, 2, 2, 3, 0, 4, 7, 2, 
+	2, 8, 0, 7, 2, 0, 9, 2, 
+	10, 11, 2
 };
 
 static const char _emoji_presentation_to_state_actions[] = {
-	0, 0, 1, 0, 0, 0, 0, 0, 
+	0, 0, 5, 0, 0, 0, 0, 0, 
 	0, 0, 0, 0
 };
 
 static const char _emoji_presentation_from_state_actions[] = {
-	0, 0, 3, 0, 0, 0, 0, 0, 
+	0, 0, 6, 0, 0, 0, 0, 0, 
 	0, 0, 0, 0
 };
 
@@ -112,7 +95,7 @@ scan_emoji_presentation (emoji_text_iter_t p,
   int cs;
 
   
-#line 116 "emoji_presentation_scanner.c"
+#line 90 "emoji_presentation_scanner.c"
 	{
 	cs = emoji_presentation_start;
 	ts = 0;
@@ -120,128 +103,63 @@ scan_emoji_presentation (emoji_text_iter_t p,
 	act = 0;
 	}
 
-#line 124 "emoji_presentation_scanner.c"
+#line 96 "emoji_presentation_scanner.c"
 	{
-	int _klen;
-	unsigned int _trans;
-	const char *_acts;
-	unsigned int _nacts;
+	int _slen;
+	int _trans;
 	const unsigned char *_keys;
-
+	const char *_inds;
 	if ( p == pe )
 		goto _test_eof;
 _resume:
-	_acts = _emoji_presentation_actions + _emoji_presentation_from_state_actions[cs];
-	_nacts = (unsigned int) *_acts++;
-	while ( _nacts-- > 0 ) {
-		switch ( *_acts++ ) {
-	case 1:
+	switch ( _emoji_presentation_from_state_actions[cs] ) {
+	case 6:
 #line 1 "NONE"
 	{ts = p;}
 	break;
-#line 143 "emoji_presentation_scanner.c"
-		}
+#line 108 "emoji_presentation_scanner.c"
 	}
 
-	_keys = _emoji_presentation_trans_keys + _emoji_presentation_key_offsets[cs];
-	_trans = _emoji_presentation_index_offsets[cs];
+	_keys = _emoji_presentation_trans_keys + (cs<<1);
+	_inds = _emoji_presentation_indicies + _emoji_presentation_index_offsets[cs];
 
-	_klen = _emoji_presentation_single_lengths[cs];
-	if ( _klen > 0 ) {
-		const unsigned char *_lower = _keys;
-		const unsigned char *_mid;
-		const unsigned char *_upper = _keys + _klen - 1;
-		while (1) {
-			if ( _upper < _lower )
-				break;
+	_slen = _emoji_presentation_key_spans[cs];
+	_trans = _inds[ _slen > 0 && _keys[0] <=(*p) &&
+		(*p) <= _keys[1] ?
+		(*p) - _keys[0] : _slen ];
 
-			_mid = _lower + ((_upper-_lower) >> 1);
-			if ( (*p) < *_mid )
-				_upper = _mid - 1;
-			else if ( (*p) > *_mid )
-				_lower = _mid + 1;
-			else {
-				_trans += (unsigned int)(_mid - _keys);
-				goto _match;
-			}
-		}
-		_keys += _klen;
-		_trans += _klen;
-	}
-
-	_klen = _emoji_presentation_range_lengths[cs];
-	if ( _klen > 0 ) {
-		const unsigned char *_lower = _keys;
-		const unsigned char *_mid;
-		const unsigned char *_upper = _keys + (_klen<<1) - 2;
-		while (1) {
-			if ( _upper < _lower )
-				break;
-
-			_mid = _lower + (((_upper-_lower) >> 1) & ~1);
-			if ( (*p) < _mid[0] )
-				_upper = _mid - 2;
-			else if ( (*p) > _mid[1] )
-				_lower = _mid + 2;
-			else {
-				_trans += (unsigned int)((_mid - _keys)>>1);
-				goto _match;
-			}
-		}
-		_trans += _klen;
-	}
-
-_match:
-	_trans = _emoji_presentation_indicies[_trans];
 _eof_trans:
 	cs = _emoji_presentation_trans_targs[_trans];
 
 	if ( _emoji_presentation_trans_actions[_trans] == 0 )
 		goto _again;
 
-	_acts = _emoji_presentation_actions + _emoji_presentation_trans_actions[_trans];
-	_nacts = (unsigned int) *_acts++;
-	while ( _nacts-- > 0 )
-	{
-		switch ( *_acts++ )
-		{
-	case 2:
-#line 1 "NONE"
-	{te = p+1;}
-	break;
-	case 3:
-#line 85 "emoji_presentation_scanner.rl"
-	{act = 2;}
-	break;
-	case 4:
-#line 86 "emoji_presentation_scanner.rl"
-	{act = 3;}
-	break;
-	case 5:
+	switch ( _emoji_presentation_trans_actions[_trans] ) {
+	case 9:
 #line 84 "emoji_presentation_scanner.rl"
 	{te = p+1;{ *is_emoji = false; return te; }}
 	break;
-	case 6:
+	case 4:
 #line 85 "emoji_presentation_scanner.rl"
 	{te = p+1;{ *is_emoji = true; return te; }}
 	break;
-	case 7:
+	case 8:
 #line 86 "emoji_presentation_scanner.rl"
 	{te = p+1;{ *is_emoji = false; return te; }}
 	break;
-	case 8:
+	case 10:
 #line 85 "emoji_presentation_scanner.rl"
 	{te = p;p--;{ *is_emoji = true; return te; }}
 	break;
-	case 9:
+	case 11:
 #line 86 "emoji_presentation_scanner.rl"
 	{te = p;p--;{ *is_emoji = false; return te; }}
 	break;
-	case 10:
+	case 3:
 #line 85 "emoji_presentation_scanner.rl"
 	{{p = ((te))-1;}{ *is_emoji = true; return te; }}
 	break;
-	case 11:
+	case 1:
 #line 1 "NONE"
 	{	switch( act ) {
 	case 2:
@@ -253,21 +171,28 @@ _eof_trans:
 	}
 	}
 	break;
-#line 257 "emoji_presentation_scanner.c"
-		}
+	case 2:
+#line 1 "NONE"
+	{te = p+1;}
+#line 85 "emoji_presentation_scanner.rl"
+	{act = 2;}
+	break;
+	case 7:
+#line 1 "NONE"
+	{te = p+1;}
+#line 86 "emoji_presentation_scanner.rl"
+	{act = 3;}
+	break;
+#line 162 "emoji_presentation_scanner.c"
 	}
 
 _again:
-	_acts = _emoji_presentation_actions + _emoji_presentation_to_state_actions[cs];
-	_nacts = (unsigned int) *_acts++;
-	while ( _nacts-- > 0 ) {
-		switch ( *_acts++ ) {
-	case 0:
+	switch ( _emoji_presentation_to_state_actions[cs] ) {
+	case 5:
 #line 1 "NONE"
 	{ts = 0;}
 	break;
-#line 270 "emoji_presentation_scanner.c"
-		}
+#line 169 "emoji_presentation_scanner.c"
 	}
 
 	if ( ++p != pe )
